7a52f351884ee182b0c962ab947a1a9295e5ee10
Hyunsoo Choi <dev.hyunsoo@gmail.com>
2016-08-05 22:52:23 -0500

    added google analytics

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,17 +1,10 @@
 $(document).ready(function() {
   $('.mobile_menu').click( function(){
-
     $('ul.nav').slideToggle();
-
   });
-
   $(window).resize(function(){
-
     if ($(window).width() > 480 ) {
-
       $('ul.nav').removeAttr('style');
-
     }
-
   });
 });
